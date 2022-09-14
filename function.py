d5446a49d230a6467fe3a0444e312fd36c3913cd
69Ali <113003602+69Ali@users.noreply.github.com>
2022-09-14 19:29:29 +0500

    Create function.py
    First commit

---
+++ function.py
@@ -0,0 +1,6 @@
+def st_detail(name,Class):
+    print(f" {name} reads in {Class} class.")
+    print(f"{name} is very brilliant in his class {Class} .")
+    print(f" {name} always stood first in his class {Class} . ")
+
+st_detail("Ali","9th")   
